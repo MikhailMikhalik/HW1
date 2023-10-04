574361a05edf41f90bcfea5e18362a48789d9536
MikhailMikhalik <132000475+MikhailMikhalik@users.noreply.github.com>
2023-10-04 23:12:14 +0300

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,24 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+        HotDrink coffee = new HotDrink("Coffee", 0.25f, 250, 80);
+        HotDrink cacao = new HotDrink("Cacao", 0.25f, 250, 50);
+        HotDrink tea = new HotDrink("Hot Tea", 0.35f, 450, 70);
+
+        List<HotDrink> drinksList = new ArrayList<>(List.of(coffee,cacao,tea));
+
+        HotDrinkVendingMachine machine = new HotDrinkVendingMachine();
+        machine.initProducts(drinksList);
+
+
+        machine.getProduct("Coffee", 0.25f, 80);
+        machine.getProduct("Coffee", 0.25f, 30);
+
+    }
+}
